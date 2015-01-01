82cf3b6727a0360ecbf29144ce57082e73fb205a
Alexander Tsoi <alexander.tsoi@outlook.com>
2015-01-01 20:39:04 +0300

    Removed assembly information version attribute

--- MBTiles/Properties/AssemblyInfo.cs
+++ MBTiles/Properties/AssemblyInfo.cs
@@ -25,5 +25,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("git")]
 [assembly: ComVisible(false)]
